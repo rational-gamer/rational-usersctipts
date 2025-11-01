dcad4787143ff71e66b6c5a4359aa453d7d8a98f
Rational gamer <thirdmaths@gmail.com>
2025-11-01 20:27:36 +0100

    ttsf v1.1.0.4 testing speed

--- tts-friendly-novelbin.user.js
+++ tts-friendly-novelbin.user.js
@@ -4,10 +4,10 @@
 // @match       https://novelbin.com/b/my-master-knows-it-all/*
 // @run-at      document-idle
 // @grant       none
-// @version     1.1.0.3
+// @version     1.1.0.4
 // @author      -
 // @description 25/10/2025 10:22:21
-// @downloadURL https://raw.githubusercontent.com/rational-gamer/rational-usersctipts/refs/heads/main/tts-friendly-novelbin.user.js?salt=v1.1.0.3
+// @downloadURL https://raw.githubusercontent.com/rational-gamer/rational-usersctipts/refs/heads/main/tts-friendly-novelbin.user.js?salt=v1.1.0.4
 // ==/UserScript==
 
 
@@ -41,7 +41,7 @@ waitForElement("#chapter .chr-title span.chr-text").then(chapterTitle => {
   console.info(`found chapter title: '${chapterTitle.textContent}'`);
   chapterTitle.textContent = chapterTitle.textContent.replace(/^\s*C(?:hapter)? ?(\d+) - [0-9 -]*(.*?)_?\d*\s*$/, 'Chapter $1 - $2')
 
-  chapterTitle.textContent = "huumf pewf hum pewft " + chapterTitle.textContent;
+  chapterTitle.textContent = "'huumf' 'pewf' 'hum' 'pewft' " + chapterTitle.textContent;
 });
 
 waitForElement("#chr-content").then(x => {
